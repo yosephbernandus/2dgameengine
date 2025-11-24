0a0c0c21bab909996b163a1164478a497c9dbd89
yosephbernandus <yosephbernandus@gmail.com>
2025-11-24 09:02:33 +0700

    movement & velocity vectors

--- src/Game.cpp
+++ src/Game.cpp
@@ -1,6 +1,7 @@
 #include "Game.h"
 #include <SDL2/SDL.h>
-#include <SDL2/SDL_render.h>
+#include <SDL2/SDL_image.h>
+#include <glm/glm.hpp>
 #include <iostream>
 
 Game::Game() {
@@ -9,19 +10,18 @@ Game::Game() {
 }
 
 Game::~Game() {
-        std::cout << "Game donstructor called" << std::endl;
+    std::cout << "Game donstructor called" << std::endl;
 }
 
 void Game::Initialize() {
     if (SDL_Init(SDL_INIT_EVERYTHING) != 0) {
-        std::cerr << "Error initializeing SDL." << std::endl;
+        std::cerr << "Error initializing SDL." << std::endl;
         return;
-    };
-
+    }
     SDL_DisplayMode displayMode;
     SDL_GetCurrentDisplayMode(0, &displayMode);
-    windowWidth = 800;// displayMode.w;
-    windowHeight = 600;// displayMode.h;
+    windowWidth = displayMode.w;
+    windowHeight = displayMode.h;
     window = SDL_CreateWindow(
         NULL,
         SDL_WINDOWPOS_CENTERED,
@@ -30,49 +30,17 @@ void Game::Initialize() {
         windowHeight,
         SDL_WINDOW_BORDERLESS
     );
-
     if (!window) {
         std::cerr << "Error creating SDL window." << std::endl;
+        return;
     }
-
     renderer = SDL_CreateRenderer(window, -1, 0);
-    if(!renderer) {
+    if (!renderer) {
         std::cerr << "Error creating SDL renderer." << std::endl;
         return;
     }
-
     SDL_SetWindowFullscreen(window, SDL_WINDOW_FULLSCREEN);
-
     isRunning = true;
-}
-
-void Game::Setup() {
-    // TODO:: initialize game objects
-}
-
-void Game::Update() {
-    
-}
-
-void Game::Render() {
-    SDL_SetRenderDrawColor(renderer, 21, 21, 21, 255);
-    SDL_RenderClear(renderer);
-
-    // Draw a rectangle
-    SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
-    SDL_Rect player = { 10, 10, 20, 20 };
-    SDL_RenderFillRect(renderer, &player);
-
-    SDL_RenderPresent(renderer);
-
-}
-
-void Game::Run() {
-    while (isRunning) {
-        ProcessInput();
-        Update();
-        Render();
-    }
 }
 
 void Game::ProcessInput() {
@@ -91,9 +59,55 @@ void Game::ProcessInput() {
     }
 }
 
+glm::vec2 playerPosition;
+glm::vec2 playerVelocity;
+
+
+void Game::Setup() {
+    playerPosition = glm::vec2(10.0, 20.0);
+    playerVelocity = glm::vec2(0.5, 0.0);
+}
+
+void Game::Update() {
+    playerPosition.x += playerVelocity.x;
+    playerPosition.y += playerVelocity.y;
+}
+
+void Game::Render() {
+    SDL_SetRenderDrawColor(renderer, 21, 21, 21, 255);
+    SDL_RenderClear(renderer);
+
+    // Draw a PNG texture
+    SDL_Surface* surface = IMG_Load("./assets/images/tank-tiger-right.png");
+    SDL_Texture* texture = SDL_CreateTextureFromSurface(renderer, surface);
+    SDL_FreeSurface(surface);
+
+    // What is destination rectangle that we want to place my texture
+    SDL_Rect dstRect = { 
+        static_cast<int>(playerPosition.x),
+        static_cast<int>(playerPosition.y),
+        32,
+        32
+    };
+    SDL_RenderCopy(renderer, texture, NULL, &dstRect);
+    
+    SDL_DestroyTexture(texture);
+
+    SDL_RenderPresent(renderer);
+
+}
+
+void Game::Run() {
+    while (isRunning) {
+        ProcessInput();
+        Update();
+        Render();
+    }
+}
 
 void Game::Destroy() {
     SDL_DestroyRenderer(renderer);
     SDL_DestroyWindow(window);
     SDL_Quit();
 }
+
