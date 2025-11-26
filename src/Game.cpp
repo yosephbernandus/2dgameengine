5b2428282bc0c2ee82f56eb4e7bbf977601c0d43
yosephbernandus <yosephbernandus@gmail.com>
2025-11-26 18:44:29 +0700

    Adding SDL Delay

--- src/Game.cpp
+++ src/Game.cpp
@@ -71,7 +71,10 @@ void Game::Setup() {
 
 void Game::Update() {
     // If too fast, waste some time until we reach the MILLISECS_PER_FRAME
-    while (!SDL_TICKS_PASSED(SDL_GetTicks(), millisecsPreviousFrame + MILLISECS_PER_FRAME));
+    int timeToWait = MILLISECS_PER_FRAME - (SDL_GetTicks() - millisecsPreviousFrame);
+    if (timeToWait > 0 && timeToWait <= MILLISECS_PER_FRAME) {
+        SDL_Delay(timeToWait);
+    }
 
     // store the current frame time
     millisecsPreviousFrame = SDL_GetTicks();
