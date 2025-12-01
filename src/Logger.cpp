7a390835818a5f1cb82c76e7eb892de27f593fbd
yosephbernandus <yosephbernandus@gmail.com>
2025-12-01 21:10:09 +0700

    Update logger to show error and warning

--- src/Logger.cpp
+++ src/Logger.cpp
@@ -1,15 +1,22 @@
 #include "Logger.h"
 #include <iostream>
+#include <chrono>
+#include <ctime>
+#include <string>
+
+std::string CurrentDateTimeToString() {
+    std::time_t now = std::chrono::system_clock::to_time_t(std::chrono::system_clock::now());
+    std::string output(30, '\0');
+    std::strftime(&output[0], output.size(), "%d-%b-%Y %H:%M:%S", std::localtime(&now));
+    return output;
+}
 
 void Logger::Log(const std::string& message) {
-    // TODO
-    // LOG: [ 12/Oct/2025 09:00:00 ] - Message
-    // Green
+    std::string output = "LOG: [" + CurrentDateTimeToString() + "]: " + message;
+    std::cout << "\x1B[32m" << output << "\033[0m" << std::endl;
 }
 
 void Logger::Err(const std::string& message) {
-    // TODO
-    // ERR: [ 12/Oct/2025 09:00:00 ] - Message
-    // Red
-
+    std::string output = "ERR: [" + CurrentDateTimeToString() + "]: " + message;
+    std::cout << "\x1B[91m" << output << "\033[0m" << std::endl;
 }
