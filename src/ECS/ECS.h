22492c66d8f66fc9aafa5d4b5b5ea76c6ad8f9a0
yosephbernandus <yosephbernandus@gmail.com>
2026-01-07 17:54:40 +0700

    ECS Folder Structure

---
+++ src/ECS/ECS.h
@@ -0,0 +1,21 @@
+#ifndef ECS_H
+#define ECS_H
+
+class Component {
+	// TODO: ....
+};
+
+class Entity {
+	// TODO: ....
+};
+
+class System {
+	// TODO: ....
+};
+
+class Registry {
+	// TODO: ....
+};
+
+
+#endif
