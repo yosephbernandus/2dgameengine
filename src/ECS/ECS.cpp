8137063f3b3aa78aebdbc2ce291db15709f6976b
yosephbernandus <yosephbernandus@gmail.com>
2026-02-15 09:52:24 +0700

    feat: systems functions

--- src/ECS/ECS.cpp
+++ src/ECS/ECS.cpp
@@ -4,3 +4,18 @@
 int Entity::GetId() const {
     return id;
 }
+
+void System::AddEntityToSystem(Entity entity) {
+
+}
+void System::RemoveEntityFromSystem(Entity entity) {
+
+}
+
+std::vector<Entity> System::GetSystemEntities() const {
+    return entities;
+}
+
+const Signature& System::GetComponentSignature() const {
+    return componentSignature;
+}
